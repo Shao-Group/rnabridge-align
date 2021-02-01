2a4f017b938017ab4a93a851fb8b4bd9ee92f196
Mingfu Shao <shaomingfu@gmail.com>
2021-01-31 19:04:17 -0500

    ready for release new version

--- src/config.cc
+++ src/config.cc
@@ -95,7 +95,7 @@ bool output_tex_files = false;
 string fixed_gene_name = "";
 int batch_bundle_size = 100;
 int verbose = 1;
-string version = "v1.0.0";
+string version = "v1.0.1";
 
 int parse_arguments(int argc, const char ** argv)
 {
